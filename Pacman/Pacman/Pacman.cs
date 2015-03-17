da08ab60390604a32033c88b76713bf0a0c95aac
João Miguel Lopes de Oliveira <joaomlo2@hotmail.com>
2015-03-17 01:31:17 +0000

    Básico do Jogo está Funcional
    (Por ordem de realização)
    -O movimento foi polido.
    -Criou-se o tabuleiro (Tabuleiro novo, diferente dos originais)
    -Adicionou-se o sistema de "Comer" e "Pontuação" (Quando as coordenadas
    do Pacman são equivalentes às posições X e Y da matriz "board" onde se
    encontra o número "1" (que identifica a comida normal), este é
    substituído pelo número "9" que identifica o espaço vazio, que por sua
    vez será representado por uma tela "muro" pintada de preto).
    
    Falta:
    -Aperfeiçoar o movimento, mais especificamente retirar por completo a
    possibilidade de movimentos diagonais que fazem com que seja possível
    entrar dentro dos muros e "cortar caminho"
    -Adicionar o elemento do "desafio", quer seja esse elemento os fantasmas
    ou o cronómetro.
    -Adicionar animações aos actores e talvez alguma música e SFX.

--- Pacman/Pacman/Pacman.cs
+++ Pacman/Pacman/Pacman.cs
@@ -9,12 +9,17 @@ namespace Pacman
     {
         public int X{get; set;}
         public int Y { get; set; }
-
+        public int boardX { get; set; }
+        public int boardY { get; set; }
+        public int Direcção { get; set; }
 
         public Pacman()
         {
-            X = 15;
-            Y = 15;
+            X = 10;
+            Y = 10;
+            boardX = 1;
+            boardY = 1;
+            Direcção = 2;
         }
     }
 }
